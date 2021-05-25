9e5fe0f5a55543b5cf0d8d61c22932d48d66b0bf
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-25 14:04:06 -0700

    ACreates, registers, and renders server component inside of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // Note the .ts extension is added by WebPack
 
+// The NgModule decorator imported from angular's core
 @NgModule({
+  // You MUST register your modules here
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// "The Component decorator imported from angular's core"
+@Component({
+  // naming convention: `app-` + file name
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+
+export class ServerComponent {
+
+}
