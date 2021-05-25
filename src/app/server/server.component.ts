ad2476d4c03f44182a0997c8e27ef79b990dbb95
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-25 15:10:34 -0700

    Playing around with string interpolation and property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,5 +9,10 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // An example of inlining template -- can also point to a file
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // // An example of inlining template -- can also point to a file
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() {
+    // Once this component is created, after two seconds, update the allowNewServer property to true
+    // Since we have established a two-way binding between the disabled property for the `Add Server` button
+    // and this.allNewServer, the Add Server button will be enabled two seconds after being loaded.
+    setTimeout(() => {
+        this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
